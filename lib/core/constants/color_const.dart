bb4b0d0c9392a4bbd16ae913af276066110ac608
Ibragimov Azizbek <ibragimovazizbek.0102@gamil.com>
2022-10-18 12:54:34 +0500

    copleted homeview

--- lib/core/constants/color_const.dart
+++ lib/core/constants/color_const.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 class ColorConst {
   static Color white = const Color(0xFFFFFFFF);
+  static Color black = const Color(0xFF000000);
   static Color primaryColor = const Color(0xFF009398);
   static Color primaryBlueAccent = const Color(0xFF00979C);
   static Color recommendColor = const Color(0xFF139D4B);
@@ -11,4 +12,6 @@ class ColorConst {
   static Color outOfStockColor = const Color(0xFFED2020);
   static Color blockColor = const Color(0xFFE2EFEF);
   static Color backgroundColor = const Color(0xFFF8F8F8);
+
+  static Color salePriceColor = const Color(0xFF848484);
 }
--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:intex/core/constants/color_const.dart';
 import 'package:intex/core/constants/font_const.dart';
 import 'package:intex/extensions/mq_extension.dart';
@@ -10,91 +9,255 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   backgroundColor: ColorConst.primaryColor,
-      //   title: Row(
-      //     mainAxisAlignment: MainAxisAlignment.spaceAround,
-      //     children: [
-      //       Text(
-      //         "INTEX-MARKET.UZ",
-      //         style: TextStyle(
-      //           color: ColorConst.white,
-      //           fontSize: FontConst.largeFont,
-      //         ),
-      //       ),
-      //       Row(
-      //         children: [
-      //           IconButton(
-      //             icon: Image.asset('assets/icons/phone.png'),
-      //             onPressed: () {},
-      //           ),
-      //           IconButton(
-      //             icon: Image.asset('assets/icons/telegram.png'),
-      //             onPressed: () {},
-      //           ),
-      //           IconButton(
-      //             icon: Image.asset('assets/icons/languageru.png'),
-      //             onPressed: () {},
-      //           ),
-      //           IconButton(
-      //             icon: Image.asset('assets/icons/menu.png'),
-      //             onPressed: () {},
-      //           ),
-      //         ],
-      //       )
-      //     ],
-      //   ),
-      // ),
+      backgroundColor: ColorConst.backgroundColor,
       body: SafeArea(
-        child: Column(
-          children: [
-            Container(
-              alignment: Alignment.center,
-              height: context.h * 0.4,
-              width: double.infinity,
-              color: ColorConst.primaryColor,
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Text(
-                        "INTEX-MARKET.UZ",
-                        style: TextStyle(
-                          color: ColorConst.white,
-                          fontSize: FontConst.largeFont,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                alignment: Alignment.center,
+                height: context.h * 0.4,
+                width: double.infinity,
+                color: ColorConst.primaryColor,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Text(
+                          "INTEX-MARKET.UZ",
+                          style: TextStyle(
+                            color: ColorConst.white,
+                            fontSize: FontConst.largeFont,
+                          ),
                         ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              IconButton(
+                                icon: Image.asset('assets/icons/phone.png',
+                                    fit: BoxFit.cover),
+                                onPressed: () {},
+                              ),
+                              IconButton(
+                                icon: Image.asset('assets/icons/telegram.png'),
+                                onPressed: () {},
+                              ),
+                              IconButton(
+                                icon:
+                                    Image.asset('assets/icons/languageru.png'),
+                                onPressed: () {},
+                              ),
+                              IconButton(
+                                icon: Image.asset('assets/icons/menu.png'),
+                                onPressed: () {},
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: context.h * 0.3,
+                      width: context.w,
+                      child: Image.asset(
+                        'assets/images/bigImage.png',
+                        fit: BoxFit.cover,
                       ),
-                      Padding(
-                        padding: EdgeInsets.symmetric(vertical: 10),
-                        child: Row(
+                    ),
+                    Text(
+                      "Сезонная распродажа!",
+                      style: TextStyle(
+                        fontSize: FontConst.extraLargeFont,
+                        color: ColorConst.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: context.h * 0.01),
+              Text(
+                "Бесплатная доставка бассейна",
+                style: TextStyle(
+                  fontSize: FontConst.meduimFont,
+                  color: ColorConst.primaryColor,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: context.h * 0.01),
+              Text(
+                "Мы ценим наших клиентов, можете быть уверены в\nкачестве нашего сервиса!",
+                style: TextStyle(
+                  fontSize: 14,
+                  color: ColorConst.primaryColor,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: context.h * 0.02),
+              Container(
+                alignment: Alignment.center,
+                height: context.h * 0.045,
+                width: context.w,
+                color: ColorConst.primaryColor,
+                child: Text(
+                  "Каркасные бассейны",
+                  style: TextStyle(
+                    fontSize: FontConst.extraLargeFont,
+                    color: ColorConst.white,
+                  ),
+                ),
+              ),
+              SizedBox(height: context.h * 0.02),
+              Stack(
+                children: [
+                  Container(
+                    height: context.h * 0.35,
+                    width: context.w * 0.8,
+                    decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.only(
+                        bottomLeft: Radius.circular(40),
+                        bottomRight: Radius.circular(40),
+                        topRight: Radius.circular(40),
+                      ),
+                      color: ColorConst.white,
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text(
+                          "Металлический каркас",
+                          style: TextStyle(
+                            fontSize: FontConst.meduimFont,
+                            color: ColorConst.primaryColor,
+                          ),
+                        ),
+                        SizedBox(
+                          height: context.h * 0.14,
+                          width: context.w * 0.5,
+                          child: Image.asset(
+                            'assets/images/bassen.png',
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
                           children: [
-                            IconButton(
-                              icon: Image.asset('assets/icons/phone.png',
-                                  fit: BoxFit.cover),
-                              onPressed: () {},
+                            Column(
+                              children: [
+                                Text(
+                                  "1.600.000 сум",
+                                  style: TextStyle(
+                                    color: ColorConst.salePriceColor,
+                                  ),
+                                ),
+                                Text(
+                                  "1.512.000 сум",
+                                  style: TextStyle(
+                                    color: ColorConst.black,
+                                  ),
+                                )
+                              ],
                             ),
-                            IconButton(
-                              icon: Image.asset('assets/icons/telegram.png'),
-                              onPressed: () {},
-                            ),
-                            IconButton(
-                              icon: Image.asset('assets/icons/languageru.png'),
-                              onPressed: () {},
-                            ),
-                            IconButton(
-                              icon: Image.asset('assets/icons/menu.png'),
-                              onPressed: () {},
+                            InkWell(
+                              child: Container(
+                                alignment: Alignment.center,
+                                height: context.h * 0.035,
+                                width: context.w * 0.25,
+                                decoration: BoxDecoration(
+                                  borderRadius: const BorderRadius.only(
+                                    bottomLeft: Radius.circular(10),
+                                    topRight: Radius.circular(10),
+                                  ),
+                                  color: ColorConst.primaryAmber,
+                                ),
+                                child: Text(
+                                  "Заказать",
+                                  style: TextStyle(color: ColorConst.black),
+                                ),
+                              ),
+                              onTap: () {
+                                // ? ZAKAZ UCHUN
+                              },
                             ),
                           ],
                         ),
-                      )
-                    ],
+                      ],
+                    ),
+                  ),
+                  Positioned(
+                    top: 1,
+                    left: 2,
+                    child: Container(
+                      alignment: Alignment.center,
+                      height: context.h * 0.03,
+                      width: context.w * 0.3,
+                      decoration: BoxDecoration(
+                        borderRadius: const BorderRadius.only(
+                          bottomRight: Radius.circular(17),
+                        ),
+                        color: ColorConst.recommendColor,
+                      ),
+                      child: Text(
+                        'Рекомендуем',
+                        style: TextStyle(color: ColorConst.white),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
                   ),
                 ],
               ),
-            )
-          ],
+              SizedBox(height: context.h * 0.02),
+              Container(
+                height: context.h * 0.25,
+                width: context.w,
+                color: ColorConst.primaryColor,
+                child: Column(
+                  children: [
+                    SizedBox(height: context.h * 0.01),
+                    Text(
+                      "Бесплатная доставка",
+                      style: TextStyle(
+                        fontSize: 25,
+                        color: ColorConst.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: context.h * 0.02),
+                    Text(
+                      "Бесплатная доставка осуществляется в пределах города Ташкент (за пределами города доставка оплачивается отдельно)",
+                      style: TextStyle(
+                        color: ColorConst.white,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(height: context.h * 0.03),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        backgroundColor: ColorConst.primaryAmber,
+                      ),
+                      child: Text(
+                        "Оформить заказ",
+                        style: TextStyle(
+                          color: ColorConst.black,
+                          fontSize: 15,
+                        ),
+                      ),
+                      onPressed: () {
+                        // ? ELEVETED BUTTUN
+                      },
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(height: context.h * 0.03),
+            ],
+          ),
         ),
       ),
     );
